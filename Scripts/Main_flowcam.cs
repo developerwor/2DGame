def186e7cda10e33baed15da8ff2bfb24907d54d
Myeongheon Han <122499446+developerwor@users.noreply.github.com>
2023-08-04 08:59:36 +0900

    Update Main_flowcam.cs

--- Scripts/Main_flowcam.cs
+++ Scripts/Main_flowcam.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main_flowcam : MonoBehaviour
-{
-    // Start is called before the first frame update
+{    
     Transform CharPos;
+    
     void Start()
     {
+        // CharPos의 변수에 현재 Player라는 태그가 붙어 있는 오브젝트의 trasnform값을 할당
         CharPos = GameObject.FindWithTag("Player").transform;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        // 카메라의 위치값이 플레이어의 x값만 따라갈수있도록 설정
         transform.position = new Vector3(CharPos.position.x, transform.position.y, transform.position.z);
     }
 }
