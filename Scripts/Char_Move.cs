6ce4e51d05786102bf4f2459d4950a417a3a72b5
Myeongheon Han <122499446+developerwor@users.noreply.github.com>
2023-08-04 09:19:26 +0900

    Update Char_Move.cs

--- Scripts/Char_Move.cs
+++ Scripts/Char_Move.cs
@@ -12,10 +12,16 @@ public class Char_Move : MonoBehaviour
     
     void Update()
     {
+        // A키 혹은 왼쪽키를 눌렀을때
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            // 왼쪽으로 이동
             Chariter.transform.Translate(new Vector3(-3,0,0) *Speed *Time.deltaTime);
+
+            // 걷는 모션 실행
             Chariter.GetComponent<Animator>().Play("walking");
+
+            // 왼쪽으로 방향 전환
             Chariter.GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -36,8 +42,10 @@ public class Char_Move : MonoBehaviour
            
         }
 
+        // 에리어를 벗어날시
         if (transform.position.y < -15)
         {
+            // 에리어 공간으로 다시 복귀
             transform.Translate(new Vector3(0,15,0));
         }
     }
