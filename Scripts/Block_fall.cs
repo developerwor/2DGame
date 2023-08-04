d4cfbc71b952c8598d33e62fd28afd555532f01a
Myeongheon Han <122499446+developerwor@users.noreply.github.com>
2023-08-04 09:30:55 +0900

    블럭 낙하

--- Scripts/Block_fall.cs
+++ Scripts/Block_fall.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Block_fall : MonoBehaviour
 {    
+    // 블럭 타입 랜덤값
     float nRandom;
+
+    // 블럭의 x값 랜덤값
     float Rd_Position;
+
+    // 랜덤 최대값
     float nMax = 0.4f;
+    
+    // 랜덤 최소값
     float nMin = -18;
 
+    //-------------블럭 위치값---------------//
     float Zero = -5;
     float Two = -1;
     float Three = -3;
-    float Four = -8;
+    float Four = -8;    
+    //-------------블럭 위치값---------------//
+    
     void Start()
     {
         nRandom = Random.RandomRange(0, 4);
@@ -22,9 +32,10 @@ public class Block_fall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //-------------블럭 낙하 스피드 타입---------------//
         if (nRandom == 0)
         {
-                transform.Translate(new Vector3(0, Zero, 0) * 1f * Time.deltaTime);                
+            transform.Translate(new Vector3(0, Zero, 0) * 1f * Time.deltaTime);                
         }
         else if (nRandom == 1)
         {
@@ -38,10 +49,18 @@ public class Block_fall : MonoBehaviour
         {
             transform.Translate(new Vector3(0, Four, 0) * 4f * Time.deltaTime);
         }
+        //-------------블럭 낙하 스피드 타입---------------//
+
+        // 블럭이 에리어를 벗어나면
         if (transform.position.y < -7)
         {
+            // 블럭의 위치 지정
             transform.position = new Vector3(Rd_Position, 10, 0);
+
+            // 블럭의 타입 랜덤지정
             nRandom = Random.RandomRange(0, 4);
+
+            // 다음 블럭의 x값 랜덤지정
             Rd_Position = Random.RandomRange(8, -9);           
         }
     }
